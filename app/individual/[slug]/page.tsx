916967200dd074ed8c4bc1b31024504cf3118774
Shohruz <freefireuzbek12@gmail.com>
2025-03-15 15:17:29 +0500

    added auto generated dynamic metadata infomation

--- app/individual/[slug]/page.tsx
+++ app/individual/[slug]/page.tsx
@@ -1,8 +1,18 @@
 import InnerDetails from '@/components/innerDetail'
+import { fetchSpecIndividual } from '@/lib'
+import { Metadata } from 'next'
 import React from 'react'
 
 type PageProps = {
   params: Promise<{ slug: string }>
+}
+
+export async function generateMetadata({ params }: PageProps): Promise<Metadata> {
+  const {slug} = await params
+  const {individual: person} = await fetchSpecIndividual(slug)
+  return {
+    title: person.fullName,
+  }
 }
 
 export default async function Individual({params}: PageProps) {
--- app/organization/[slug]/page.tsx
+++ app/organization/[slug]/page.tsx
@@ -1,7 +1,17 @@
 import InnerOrganization from "@/components/specOrganization"
+import { fetchSpecOrganization } from "@/lib"
+import { Metadata } from "next"
 
 type PageProps = {
   params: Promise<{ slug: string }>
+}
+
+export async function generateMetadata({ params }: PageProps): Promise<Metadata> {
+  const {slug} = await params
+  const {organization} = await fetchSpecOrganization(slug)
+  return {
+    title: organization.name,
+  }
 }
 
 export default async function Organization({params}: PageProps) {
