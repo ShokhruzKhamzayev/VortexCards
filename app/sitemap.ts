e3c7bb3f13c4e77540bb4e903232f6ba5a7779ca
Shohruz <freefireuzbek12@gmail.com>
2025-03-30 18:30:53 +0500

    changed sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,18 +1,8 @@
-import { fetchAllIndividuals } from "@/lib";
 import { MetadataRoute } from "next";
 
 const baseUrl = 'https://cards.vortexhub.uz/'
 
 export async function sitemap(): Promise<MetadataRoute.Sitemap> {
-    const {individuals} = await fetchAllIndividuals()
-    const data = individuals.map(individual => {
-        return {
-            url: `${baseUrl}${individual.slug}`,
-            priority: 0.5,
-            images: [individual.avatar.url],
-            lastModified: individual.createdAt
-        }
-    })
     return [
         {
             url: baseUrl,
