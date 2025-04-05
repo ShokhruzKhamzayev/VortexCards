085f3511b0ef54184187f6f0ca24d9b01c19a5b5
Shohruz <freefireuzbek12@gmail.com>
2025-04-05 14:11:17 +0500

    added seo and search console

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,18 @@ export const metadata: Metadata = {
     default: 'VortexCards | NFC equipments',
     template: '%s | VortexCards'
   },
-  description: "VortexCards bu sizning biznessingiz uchun NFC karta, braslet va stikerlarni taqdim qiladi.",
+  description: "VortexCards bu sizning biznessingiz uchun eski qog'oz tashrif qog'ozlaringizni NFC karta, braslet va stikerlarga almashtiradi.",
+  keywords: ['Onlayn Visitka', 'Visitka web sayt', 'Tashrif qog\'oz onlayn', 'Vortex Cards visitkalar', 'VortexCards'],
+  authors: [{name: 'Shaxruzbek Xamzayev', url: 'https://khamzayev.uz'}, {name: 'Sardorbek Hamidov', url: 'https://cards.vortexhub.uz/individual/hamidov-sardorbek'}],
+  category: 'online service',
+  openGraph: {
+    title: "VortexCards | Sizning onlayn tashrif qog'ozingiz",
+    description: "VortexCards bu sizning biznessingiz uchun eski qog'oz tashrif qog'ozlaringizni NFC karta, braslet va stikerlarga almashtiradi.",
+  },
+  verification: {
+    google: '_kN7_GGe85pNxxhU74HNL5P85ge2cApZBFHryUzjTAw',
+    yandex: '8a7036e97c55ddf4'
+  }
 };
 
 export default function RootLayout({
