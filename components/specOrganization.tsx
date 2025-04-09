119ad91c8509fca63e2e353169a10221b34db8bd
Shohruz <freefireuzbek12@gmail.com>
2025-04-09 01:35:58 -0700

    added fn: debug part #2

--- components/specOrganization.tsx
+++ components/specOrganization.tsx
@@ -20,6 +20,7 @@ import { Social } from './social'
 import ThemeSwitcher from './themeSwitcher'
 import { EmblaCarousel } from './ui/carousel'
 import ScrollToTopShared from './ui/scrollToTop'
+import DownloadPdfButton from './pdf/customDownload'
 
 export default async function InnerDetails({slug}: {slug: string}) {
     const {organization} = await fetchSpecOrganization(slug)
@@ -49,6 +50,7 @@ export default async function InnerDetails({slug}: {slug: string}) {
                                 <CustomImage src={organization.avatar.url} alt={organization.name} classname='rounded-full border border-white'/>
                             </div>
                             </div>
+                            <DownloadPdfButton data={organization}/>
                             <a href={organization.location} className="location absolute top-[100%] right-1/8  translate-y-[-50%] flex items-center flex-col">
                                 <div className={`p-[15px] rounded-full`} style={{backgroundColor: organization.secondaryColor.hex}}>
                                     <FaLocationDot color='white' />
