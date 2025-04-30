72bb5bedcd453cc613715caddb592904852db557
Shohruz <freefireuzbek12@gmail.com>
2025-04-30 18:57:43 +0500

    added static loading and not found translations

--- frontend/app/[locale]/not-found.tsx
+++ frontend/app/[locale]/not-found.tsx
@@ -3,9 +3,11 @@ import dynamic from 'next/dynamic';
 
 import animation from '../../public/animation.json'
 import Link from 'next/link';
+import { useTranslations } from 'next-intl';
 const Lottie = dynamic(() => import("react-lottie-player"), { ssr: false });
 
 export default function NotFound() {
+  const t = useTranslations("notFound")
   return (
     <div className='custom-container'>
         <div className='max-w-[500px] mx-auto text-center min-h-[100dvh] flex justify-center items-center flex-col gap-[15px]'>
@@ -15,8 +17,8 @@ export default function NotFound() {
             play
             className='w-full'
             />
-            <h1 className='text-[25px] md:text-[32px] font-semibold tracking-wide'>Internetda yo'qolib qoldingiz!</h1>
-            <Link className='bg-black dark:bg-white py-[12px] px-[17px] text-white dark:text-black rounded-[13px]' href={'/'}>Bosh sahifaga qaytish</Link>
+            <h1 className='text-[25px] md:text-[32px] font-semibold tracking-wide'>{t("title")}</h1>
+            <Link className='bg-black dark:bg-white py-[12px] px-[17px] text-white dark:text-black rounded-[13px]' href={'/'}>{t("button")}</Link>
         </div>
     </div>
   )
