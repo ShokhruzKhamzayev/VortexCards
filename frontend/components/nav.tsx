32740cce51ccab19201021a15b5a4d8ac32c7590
Shohruz <freefireuzbek12@gmail.com>
2025-05-07 13:20:16 +0500

    added locale attribute to links in navbar

--- frontend/components/nav.tsx
+++ frontend/components/nav.tsx
@@ -3,19 +3,21 @@
 import { cn } from "@/lib/utils";
 import { useTranslations } from "next-intl";
 import Link from "next/link";
+import { useParams } from "next/navigation";
 
 export default function Nav({className}: {
   className?: string,
 }) {
   const t = useTranslations();
+  const params = useParams<{locale: string}>()
   return (
     <nav className={cn("space-x-[20px]", className)}>
-      <Link href={'/#main'}>{t("home.nav.main")}</Link>
-      <Link href={'/#info'}>{t("home.nav.info")}</Link>
-      <Link href={'/#plans'}>{t("home.nav.plan")}</Link>
-      <Link href={'/#users'}>{t("home.nav.cliets")}</Link>
-      <Link href={'/#usage'}>{t("home.nav.how")}</Link>
-      <Link href={'/#contact'}>{t("home.nav.contact")}</Link>
+      <Link locale={params.locale} href={'/#main'}>{t("home.nav.main")}</Link>
+      <Link locale={params.locale} href={'/#info'}>{t("home.nav.info")}</Link>
+      <Link locale={params.locale} href={'/#plans'}>{t("home.nav.plan")}</Link>
+      <Link locale={params.locale} href={'/#users'}>{t("home.nav.cliets")}</Link>
+      <Link locale={params.locale} href={'/#usage'}>{t("home.nav.how")}</Link>
+      <Link locale={params.locale} href={'/#contact'}>{t("home.nav.contact")}</Link>
     </nav>
   )
 }
