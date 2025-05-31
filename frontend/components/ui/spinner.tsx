c6fc2faefc3fa3eb26b089eb31170dc199547041
Shohruz <freefireuzbek12@gmail.com>
2025-05-31 14:21:33 +0500

    client and server component opposition fixed

--- frontend/components/ui/spinner.tsx
+++ frontend/components/ui/spinner.tsx
@@ -1,7 +1,8 @@
-import { getTranslations } from "next-intl/server"
+'use client'
+import { useTranslations } from "next-intl"
 
-export default async function Spinner() {
-  const t = await getTranslations()
+export default function Spinner() {
+  const t = useTranslations()
   return (
     <div role="status" className='flex gap-[8px] min-h-[100dvh] w-full justify-center items-center'>
       <svg aria-hidden="true" className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
